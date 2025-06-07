1fb040d6b879b08b82689aa2942e2685a17643ca
GioPedo <103187105+GioPedo@users.noreply.github.com>
2025-06-07 10:47:36 +0200

    Update and rename dcc_garch_models.m to dcc_main_driver.m

--- code/dcc_garch_models.m
+++
@@ -1,2 +0,0 @@
-% SPECIFICARE MFE TOOLBOX, descrivere in anteprima che vengono calcolate le correlazioni rolling-window, oppure creare altri due fold dove in uno si fa il confronto
-% con rolling, uno dove vengono calcolati tutti i modelli dcc, e un altro dove di confronta quello scelto con quello che esclude la media. Poi creare un file readme
