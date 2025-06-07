42ec891d0a8abcf417ab2e406dc9840d3eab612f
GioPedo <103187105+GioPedo@users.noreply.github.com>
2025-06-07 11:10:42 +0200

    Update dcc_compute_rolling.m

--- code/dcc_garc_models/dcc_compute_rolling.m
+++ code/dcc_garc_models/dcc_compute_rolling.m
@@ -13,10 +13,12 @@
 ndx_synt = [index_macro, msft, aapl, nvda, amzn, crwd, ea, intu, pypl];
 stocks_names = {'MSFT','AAPL','NVDA','AMZN','CRWD','EA','INTU','PYPL'};
 
+
 %% 2. Rolling Correlation Parameters
 window_size = 90;
 total_length = length(ndx_synt);
 rolling_corr = NaN(total_length, 8);  % 8 asset correlations
+
 
 %% 3. Compute Rolling Correlations
 for i = 1:total_length
@@ -26,10 +28,12 @@ for i = 1:total_length
     rolling_corr(i, :) = corr(window_data(:, 1), window_data(:, 2:end));
 end
 
+
 %% 4. Convert to Timetable
 Fulldates_datetime = datetime(Fulldates);
 rolling_corr_table = array2timetable(rolling_corr, 'RowTimes', Fulldates_datetime);
 rolling_corr_table.Properties.VariableNames = stocks_names;
+
 
 
 # =============================================================================
